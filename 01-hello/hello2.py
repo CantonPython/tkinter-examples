a63af8436013483b96f4c72f2cc0643824a78487
Michael Meffie <mmeffie@sinenomine.net>
2019-04-25 20:59:16 -0400

    Say hello in the window

--- 01-hello/hello2.py
+++ 01-hello/hello2.py
@@ -5,15 +5,17 @@ class MyApplication:
     def __init__(self, parent):
         frame = Frame(parent)
         frame.pack()
-        button1 = Button(frame, text='Quit', command=frame.quit)
-        button1.pack(side=LEFT)
-        button2 = Button(frame, text='Hello', command=self.hello)
-        button2.pack(side=LEFT)
+        Button(frame, text='Quit', command=frame.quit).pack()
+        Button(frame, text='Hello', command=self.say_hello).pack()
+        self.label = Label(frame, text='')
+        self.label.pack()
 
-    def hello(self):
+    def say_hello(self):
         print('Hello World')
+        self.label['text'] = 'Hello World'
 
 root = Tk()
-app = MyApplication(root)
+root.geometry('200x200+100+100')
+MyApplication(root)
 root.mainloop()
 root.destroy()
