85fb6771508497b95be971de34edcf9801f27e8a
Michael Meffie <mmeffie@sinenomine.net>
2019-05-06 11:50:09 -0400

    Remove default start of range arguments

--- 04-password-generator/2/passgen.py
+++ 04-password-generator/2/passgen.py
@@ -4,17 +4,17 @@ import random
 
 def _generate(length, upper=0, digits=0, symbols=0):
     pw = []
-    for _ in range(0, upper):
+    for _ in range(upper):
         c = random.choice(string.ascii_uppercase)
         pw.append(c)
-    for _ in range(0, digits):
+    for _ in range(digits):
         c = random.choice(string.digits)
         pw.append(c)
-    for _ in range(0, symbols):
+    for _ in range(symbols):
         c = random.choice(string.punctuation)
         pw.append(c)
     length = length - len(pw)
-    for _ in range(0, length):
+    for _ in range(length):
         c = random.choice(string.ascii_lowercase)
         pw.append(c)
     random.shuffle(pw)
